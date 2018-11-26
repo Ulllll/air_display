4e2f75b288187b085832d1b927546752a1c730a6
Ulllll <nooryes1@gmail.com>
2018-11-26 13:01:45 +0300

    сбор массива отправления

--- ajax_get.js
+++ ajax_get.js
@@ -1,16 +1,16 @@
 
 const baseUrl = 'https://api.rasp.yandex.net/v3.0/schedule/?'
 const apikey = 'b1f9bae6-848a-42a1-b7cd-3b9790ac360f'
-var oneElement
 $.ajax({
     
-          url: `${baseUrl}apikey=${apikey}&station=s9600213&transport_type=plane`,
+          url: `${baseUrl}apikey=${apikey}&station=s9600213&transport_type=plane&event=departure`,
           success: function(data) {
             oneElement = [
-                fromTo=data.schedule.[0].days,
-                nameCompany=data.schedule.0.thread.title,
-                fromTo=data.schedule.0.days
+                time=data.schedule[0].departure,
+                company=data.schedule[0].thread.carrier.title,
+                fromTo=data.schedule[0].thread.title,
+                number=data.schedule[0].thread.number
                 ]
-            console.log(data)
+            console.log(oneElement)
           }
         })
