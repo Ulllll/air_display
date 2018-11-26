2e728f03f3795cd1f70bc07484b35a0d0b9dfad6
Ulllll <nooryes1@gmail.com>
2018-11-26 20:42:19 +0300

    внешний вид, footer и header

---
+++ footer.js
@@ -0,0 +1,3 @@
+$('footer').append(`<hr><div class="footer"><div><img src="img/logo.png" alt="Логотип"></div>
+<div class="contact"><p>+9(999)999-99-99</p>
+<p>Example@example.ru</p></div></div>`)
---
+++ header.js
@@ -0,0 +1,3 @@
+$('header').append(`<div><img src="img/logo.png" alt="Логотип"></div><div><p class="name">Аэропорт "Название"</p></div>
+<div class="contact"><p>+9(999)999-99-99</p>
+<p>Example@example.ru</p></div>`)
