98db3ed46edf8fffee489378d897d7f3353664ab
Ulllll <nooryes1@gmail.com>
2018-11-26 14:23:50 +0300

    информация о вылетах по порядку

--- ajax_get.js
+++ ajax_get.js
@@ -1,16 +1,34 @@
 
 const baseUrl = 'https://api.rasp.yandex.net/v3.0/schedule/?'
 const apikey = 'b1f9bae6-848a-42a1-b7cd-3b9790ac360f'
+var k=[]
 $.ajax({
     
           url: `${baseUrl}apikey=${apikey}&station=s9600213&transport_type=plane&event=departure`,
-          success: function(data) {
-            oneElement = [
-                time=data.schedule[0].departure,
-                company=data.schedule[0].thread.carrier.title,
-                fromTo=data.schedule[0].thread.title,
-                number=data.schedule[0].thread.number
+          success: function(data) { 
+            for(i=0;i<100;i++)
+            {   
+                $('#go').append(`<article id=\'goArticle${i}\'></article>`)
+                oneElement = [
+                time=data.schedule[i].departure,
+                company=data.schedule[i].thread.carrier.title,
+                fromTo=data.schedule[i].thread.title,
+                number=data.schedule[i].thread.number
                 ]
-            console.log(oneElement)
+                k[i] = number
+                if ((k[i] == 0)&(k[i] ==1)){
+                    $(`#goArticle${i}`).append(`<p>${fromTo}</p>`)
+                    $(`#goArticle${i}`).append(`<p>${time}</p>`)
+                    $(`#goArticle${i}`).append(`<p>${company}</p>`)
+                    $(`#goArticle${i}`).append(`<p>${number}</p>`)
+                }
+                if((k[i]!=k[i-1])&(k[i]!=k[i-2])){
+                    $(`#goArticle${i}`).append(`<p>${fromTo}</p>`)
+                    $(`#goArticle${i}`).append(`<p>${time}</p>`)
+                    $(`#goArticle${i}`).append(`<p>${company}</p>`)
+                    $(`#goArticle${i}`).append(`<p>${number}</p>`)
+                }
+            
+            }
           }
         })
