2991990b281519013828c00df2535b8a8753947c
Ulllll <nooryes1@gmail.com>
2018-11-27 19:27:11 +0300

    редактирование footer+header

--- footer.js
+++ footer.js
@@ -1,3 +1,3 @@
-$('footer').append(`<hr><div class="footer"><div><img src="img/logo.png" alt="Логотип"></div>
+$('footer').append(`<hr><div class="footer"><div><a href="#"><img src="img/logo.png" alt="Логотип"></a></div>
 <div class="contact"><p>+9(999)999-99-99</p>
 <p>Example@example.ru</p></div></div>`)
--- header.js
+++ header.js
@@ -1,3 +1,3 @@
-$('header').append(`<div><img src="img/logo.png" alt="Логотип"></div><div><p class="name">Аэропорт "Название"</p></div>
+$('header').append(`<div><a href="#"><img src="img/logo.png" alt="Логотип"></a></div><div><p class="name">Аэропорт "Название"</p></div>
 <div class="contact"><p>+9(999)999-99-99</p>
 <p>Example@example.ru</p></div>`)
